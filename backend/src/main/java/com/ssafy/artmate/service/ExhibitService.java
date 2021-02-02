2e60425a4493e5b8e6a90908a87f36fdea6863e6
dovvn <jdaun.dev@gmail.com>
2021-02-02 23:54:31 +0900

    BE_0202: 서울시 공공데이터 파싱 완료(ExhibitApiController)

--- backend/src/main/java/com/ssafy/artmate/service/ExhibitService.java
+++ backend/src/main/java/com/ssafy/artmate/service/ExhibitService.java
@@ -1,5 +1,12 @@
 package com.ssafy.artmate.service;
 
+import java.util.HashMap;
+
+import com.ssafy.artmate.Dto.ExhibitDto;
+
 public interface ExhibitService {
+	ExhibitDto selectExhibitBySeq(String seq); //일련번호에 해당하는 전시회 찾기
+	boolean insertExhibit(ExhibitDto dto); //전시회 데이터 넣기
+	boolean insertTag(HashMap<String, Integer> tagInfo); //전시회 태그정보 넣기
 
 }
