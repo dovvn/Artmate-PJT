ea418f95567e7f4ef367d72f92dd1eb4b2c54be8
dovvn <jdaun.dev@gmail.com>
2021-02-04 16:20:10 +0900

    BE_0204: 전시회 DTO 수정

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: "http://localhost:7777/",
+  baseURL: "http://i4b202.p.ssafy.io:7777/",
   headers: {
     "Content-type": "application/json"
   }
