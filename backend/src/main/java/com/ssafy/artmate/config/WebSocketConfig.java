c574730e735ba81221b64c059a6f2114ffc6f49a
hyeji6138 <hyeji6138@naver.com>
2021-02-09 15:20:47 +0900

    BE_0209 : 전시회 알림 소켓통신 추가 (SocketController)

--- backend/src/main/java/com/ssafy/artmate/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/artmate/config/WebSocketConfig.java
@@ -16,8 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/socket").setAllowedOrigins("http://localhost:8080").withSockJS();
-		//http://i4b202.p.ssafy.io
+		registry.addEndpoint("/socket").setAllowedOrigins("http://i4b202.p.ssafy.io").withSockJS();
 	}
 	
 }
