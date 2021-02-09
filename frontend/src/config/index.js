3498381980f7da4c19b4fac1012555addc1175f6
유진이 <wlsdk400@naver.com>
2021-02-09 16:05:36 +0900

    FE_0209:베이스url돌려놈

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'http://localhost:7777/';
+const API_BASE_URL = 'http://i4b202.p.ssafy.io/';
 
 export {API_BASE_URL};
--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios 객체 생성  
 export default axios.create({
-  baseURL: "http://localhost:7777/",
+  baseURL: "http://i4b202.p.ssafy.io/",
   headers: {
     "Content-type": "application/json"
   }
