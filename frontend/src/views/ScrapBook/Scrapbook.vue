9f72d391c438b01617f0faef6715a58b315ff543
dovvn <jdaun.dev@gmail.com>
2021-02-11 08:33:37 +0900

    merge

--- frontend/src/views/ScrapBook/Scrapbook.vue
+++ frontend/src/views/ScrapBook/Scrapbook.vue
@@ -1,97 +1,80 @@
 <template>
-  <div id="app" class="scrapdiv">
-    <link rel="stylesheet" href="vue-booklet.min.css">
-    <div>
-      <Navi class="scrap__navi" />
-    </div>
-    <div class="container">
+  <div id="app">
+    <link rel="stylesheet" href="vue-booklet.min.css" />
+    <Navi class="scrap__navi" />
+    <div id="container">
       <h1>Scrapbook</h1>
       <p>당신만의 "아트 스크랩북"을 만들어보세요😉</p>
-
- <Book>
-    <div class="page cover">
-      <article class="content">
-        <h1>My first book</h1>
-      </article>
-    </div>
-    <div class="page">
-      <article class="content">
-        <h1>Hello World !</h1>
-        <p>Using vue-booklet to create a book which can fliped.</p>
-        <p>Please feel free to use it.</p>
-      </article>
-    </div>
-  </Book>
-
     </div>
   </div>
 </template>
 
 <script>
 import Navi from "@/components/Common/Navi.vue";
-import booklet from 'vue-booklet';
-
-
 
 export default {
   name: "Scrapbook",
   components: {
     Navi,
-    Book: booklet
   },
   data: () => {
-    return {
-
-    };
+    return {};
   },
-  methods: {
-
-  },
-  mounted: function() {
-
-  },
+  methods: {},
+  mounted: function() {},
 };
 </script>
 
 <style>
 @import "../../components/css/style.css";
 
-
 * {
   font-family: NanumBarunGothic;
   box-sizing: border-box;
 }
-.scrapdiv {
-  width: 380px;
-  height: 100%;
-  text-align: center;
+#app {
+  width: 760px;
+  height: 320px;
+  text-align: left;
   margin: 0 auto;
+  background-color: #313030;
 }
 
-.scrap__navi {
-  width: 380px;
-  height: auto;
+.scrap__navi{
+
 }
 
-.container {
+#container {
   float: left;
   margin-top: 70px;
   width: 380px;
   height: auto;
 }
 
-.container h1 {
-  float: left;
+#container > h1 {
   font-size: 24px;
   padding: 32px 0 0 30px;
   font-size: 26px;
   color: #151515;
 }
 
-.container p {
-  float: left;
+#container > p {
   padding: 0 0 20px 30px;
   font-size: 12px;
   color: #666;
 }
+
+/* ------------------------------ 반응형 ------------------------------ */
+@media screen and (max-width: 1024px) {
+  #app {
+    width: 380px;
+  }
+}
+
+/*
+@media screen and (max-width: 1024px) {
+
+  }
+}
+*/
 </style>
