515f06ea462e7e93c0ee4933fd6d6471cc1ed9d8
유진이 <wlsdk400@naver.com>
2021-02-17 18:23:09 +0900

    FE_0217:인기피드 불러짐, 슬롯에러 해결

--- frontend/src/views/Home/Home.vue
+++ frontend/src/views/Home/Home.vue
@@ -317,14 +317,14 @@ export default {
       }
     )
   },
-  mounted(){
-      console.log(this.$refs.mycarousel.$children[0]);
-      console.log(this.$refs.mycarousel.$children[this.$refs.mycarousel.currentIndex].$slots.default[0].elm.classList);
-      console.log(this.$refs.mycarousel.currentIndex);
-      this.$refs.mycarousel.currentIndex=0;
-      this.$refs.mycarousel.$children[this.$refs.mycarousel.currentIndex].$slots.default[0].elm.classList.remove('a');
-      this.$refs.mycarousel.$children[this.$refs.mycarousel.currentIndex].$slots.default[1].elm.classList.remove('b');
-  },
+  // mounted(){
+  //     console.log(this.$refs.mycarousel.$children[0]);
+  //     console.log(this.$refs.mycarousel.$children[this.$refs.mycarousel.currentIndex].$slots.default[0].elm.classList);
+  //     console.log(this.$refs.mycarousel.currentIndex);
+  //     this.$refs.mycarousel.currentIndex=0;
+  //     this.$refs.mycarousel.$children[this.$refs.mycarousel.currentIndex].$slots.default[0].elm.classList.remove('a');
+  //     this.$refs.mycarousel.$children[this.$refs.mycarousel.currentIndex].$slots.default[1].elm.classList.remove('b');
+  // },
   destroyed(){
     document.removeEventListener('scroll',this.handleNavi);
   },
