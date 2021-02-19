b6e74934795d8a6793c5be9502116fda0b4addc0
dovvn <jdaun.dev@gmail.com>
2021-02-19 10:29:54 +0900

    FE_0219: 취향없을때 안내

--- frontend/src/components/Exhibition/ExhibitList.vue
+++ frontend/src/components/Exhibition/ExhibitList.vue
@@ -84,9 +84,6 @@ export default {
       user.userId,
       (res)=>{
         this.ex_list=res.data;
-        for(var i=0; i<this.ex_list.length; i++){
-          console.log("사진주소 "+this.ex_list[i].exImg);
-        }
         this.initMap();
       },
       (err)=>{
